68122e67fad1e48e3b815b4fd9b90c1d89d16822
Ali Jafarian <alipj333@gmail.com>
2015-09-10 20:58:56 -0600

    Adding pagination for posts list

--- server/publications.js
+++ server/publications.js
@@ -1,12 +1,21 @@
+// query all posts
 Meteor.publish('posts', function() {
 	return Posts.find();
 });
 
+// query individual posts
+Meteor.publish('singlePost', function(id) {
+	check(id, String);
+	return Posts.find(id);
+});
+
+// query comments based on post id
 Meteor.publish('comments', function(postId) {
   	check(postId, String);
   	return Comments.find({postId: postId});
 });
 
+// query notifications based on user and read status
 Meteor.publish('notifications', function() {
 	return Notifications.find({userId: this.userId, read: false});
 });
